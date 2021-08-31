1e562cb0f88d5fe1dec25d52a9d2b3ba7373eea6
droog126 <59406205+droog126@users.noreply.github.com>
2021-08-31 17:59:21 +0800

    testgi

--- git.mjs
+++ git.mjs
@@ -1,7 +1,8 @@
 var name='tool'
-var url='https://github.com/droog126/react-component.git'
+var url='https://github.com/droog126/code-tool'
 await $`git init`
 await $`git add *`
 await $`git commit -m "testgi"`
-// await $`git remote add ${name} ${url}`
+await $`git remote rm ${name}`
+await $`git remote add ${name} ${url}`
 await $`git push ${name} master`
